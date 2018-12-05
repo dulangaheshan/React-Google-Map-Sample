09ccb6c2776d6555b07614d445ad07411caafa1b
Dulanga Heshan <164056m@uom.lk>
2018-12-05 08:29:29 +0530

    use app.js as your location component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,96 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Map, InfoWindow, Marker, GoogleApiWrapper } from "google-maps-react";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      lat: null,
+      lng: null,
+      markers: []
+    };
+  }
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(position =>
+      this.setState({
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      })
+    );
+  }
+
+  mapClicked = event => {
+    const { markers } = this.state;
+    this.setState({
+      markers: [
+        {
+          position: event.latLng,
+          key: Date.now(),
+          defaultAnimation: 2
+        },
+        ...markers
+      ]
+    });
+    console.log(markers);
+  };
+
+  onMarkerDragEnd = coord => {
+    const { latLng } = coord;
+    const lat = latLng.lat();
+    const lng = latLng.lng();
+
+    this.setState({
+      lat: lat,
+      lng: lng
+    });
+
+    console.log(lat, lng);
+    // this.setState(prevState => {
+    //   // const markers = [...this.state.markers];
+    //   // markers[index] = { ...markers[index], position: { lat, lng } };
+    //   // return { markers };
+    // });
+  };
+
   render() {
+    if (!this.props.loaded) {
+      return <div>Loading...</div>;
+    }
+    const style = {
+      width: "100%",
+      height: "100vh"
+    };
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Map
+        google={this.props.google}
+        zoom={11}
+        style={style}
+        initialCenter={{
+          lat: this.state.lat,
+          lng: this.state.lng
+        }}
+        center={{
+          lat: this.state.lat,
+          lng: this.state.lng
+        }}
+        onClick={this.mapClicked}
+      >
+        <Marker
+          title={"Geolocation"}
+          position={{
+            lat: this.state.lat,
+            lng: this.state.lng
+          }}
+          draggable={true}
+          onDragend={(t, map, coord) => this.onMarkerDragEnd(coord)}
+        />
+      </Map>
     );
   }
 }
 
-export default App;
+export default GoogleApiWrapper({
+  apiKey: "AIzaSyCZ7rgMN34kWkGvr8Pzkf_8nkT7W6gowBA"
+})(App);
